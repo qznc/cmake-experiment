6c15a4d674fd9bd4acf534a4e9dab5a0e24bb6fd
Andreas Zwinkau <zwinkau@mailbox.org>
2021-03-12 19:52:47 +0100

    Make add and sub depend on rangecheck
    So we have a diamond dependency shape.
    Sadly, it requires some include-guard like cmake coding.

--- add/src/add.cpp
+++ add/src/add.cpp
@@ -1,6 +1,9 @@
 #include "add/add.hpp"
+#include "rangecheck/rangecheck.hpp"
 
 int add2(int x)
 {
+  if (!isPositive(x))
+    return 0;
   return x + 2;
 }
--- sub/src/sub.cpp
+++ sub/src/sub.cpp
@@ -1,6 +1,9 @@
 #include "sub/sub.hpp"
+#include "rangecheck/rangecheck.hpp"
 
 int sub2(int x)
 {
+  if (!isPositive(x))
+    return 0;
   return x - 2;
 }
